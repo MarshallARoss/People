4c1d49405825a8a712dd74c2383eab0e44fca96d
Marshall A Ross <MarshallARoss@gmail.com>
2022-08-09 11:06:59 -0700

    Added enum for tab symbols

---
+++ Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  People
+//
+//  Created by Marshall  on 8/9/22.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.1")
+    static let geat = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
+
+
