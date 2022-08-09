fe3f0b1736b9c45d72bee06ef7d84620a213438a
Marshall A Ross <MarshallARoss@gmail.com>
2022-08-09 10:51:31 -0700

    First commit to People

---
+++ People/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  People
+//
+//  Created by Marshall  on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to the People App")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Marshall  on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
