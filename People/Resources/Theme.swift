66baedb4656b9e2d384db0a0adf7b817dc725968
Marshall A Ross <MarshallARoss@gmail.com>
2022-08-09 11:14:42 -0700

    Added Color enum

---
+++ People/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  People
+//
+//  Created by Marshall  on 8/9/22.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detailBackground")
+    static let cardAccent = Color("cardAccent")
+    static let text = Color("text")
+
+}
